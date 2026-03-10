af227de4a6da367974a232a80a3e1e1b9e4269a3
przemyslaw_dyjak <przemek.dyjak2@gmail.com>
2026-03-10 18:12:50 +0100

    walkaaaa

---
+++ build/CMakeFiles/pea_zad1.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pea_zad1.dir/src/BruteForceSolver.cpp.o"
+  "CMakeFiles/pea_zad1.dir/src/BruteForceSolver.cpp.o.d"
+  "CMakeFiles/pea_zad1.dir/src/CSVWriter.cpp.o"
+  "CMakeFiles/pea_zad1.dir/src/CSVWriter.cpp.o.d"
+  "CMakeFiles/pea_zad1.dir/src/Config.cpp.o"
+  "CMakeFiles/pea_zad1.dir/src/Config.cpp.o.d"
+  "CMakeFiles/pea_zad1.dir/src/FileReader.cpp.o"
+  "CMakeFiles/pea_zad1.dir/src/FileReader.cpp.o.d"
+  "CMakeFiles/pea_zad1.dir/src/Generator.cpp.o"
+  "CMakeFiles/pea_zad1.dir/src/Generator.cpp.o.d"
+  "CMakeFiles/pea_zad1.dir/src/InstanceListReader.cpp.o"
+  "CMakeFiles/pea_zad1.dir/src/InstanceListReader.cpp.o.d"
+  "CMakeFiles/pea_zad1.dir/src/MemoryUsage.cpp.o"
+  "CMakeFiles/pea_zad1.dir/src/MemoryUsage.cpp.o.d"
+  "CMakeFiles/pea_zad1.dir/src/NNSolver.cpp.o"
+  "CMakeFiles/pea_zad1.dir/src/NNSolver.cpp.o.d"
+  "CMakeFiles/pea_zad1.dir/src/OptTourReader.cpp.o"
+  "CMakeFiles/pea_zad1.dir/src/OptTourReader.cpp.o.d"
+  "CMakeFiles/pea_zad1.dir/src/RNNSolver.cpp.o"
+  "CMakeFiles/pea_zad1.dir/src/RNNSolver.cpp.o.d"
+  "CMakeFiles/pea_zad1.dir/src/RandSolver.cpp.o"
+  "CMakeFiles/pea_zad1.dir/src/RandSolver.cpp.o.d"
+  "CMakeFiles/pea_zad1.dir/src/TourUtils.cpp.o"
+  "CMakeFiles/pea_zad1.dir/src/TourUtils.cpp.o.d"
+  "CMakeFiles/pea_zad1.dir/src/main.cpp.o"
+  "CMakeFiles/pea_zad1.dir/src/main.cpp.o.d"
+  "pea_zad1"
+  "pea_zad1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pea_zad1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/pea_zad1.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for pea_zad1.
+# This may be replaced when dependencies are built.
